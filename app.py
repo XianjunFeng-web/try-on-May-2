f08449abeb75264c64feff2b865f204d1e06c15e
XianjunFeng-web <fengxianjun000@hotmail.com>
2026-05-02 11:17:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 Import streamlit as st
-st.write （“ISOM5240”）
-st.write （“ISOM5240”）
+st.write（'ISOM5240'）
+st.write（'ISOM5240'）
+st.write（'ISOM5240'）
 
-st.write （“ISOM5240”）
-st.write （“ISOM5240”）
